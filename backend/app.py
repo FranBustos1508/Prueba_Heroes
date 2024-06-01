d6d39e7de78f7f75ca3b40e5e1c1e1a59a61a228
Franco <francoabustos1508@gmail.com>
2024-05-31 22:11:39 -0300

    Avance página de cada héroe

--- backend/app.py
+++ backend/app.py
@@ -1,6 +1,6 @@
 from flask import Flask
 from flask_cors import CORS
-from db.characters import get_all_characters
+from db.characters import get_all_characters, get_character_by_id
 
 app = Flask(__name__)
 CORS(app)
@@ -8,3 +8,7 @@ CORS(app)
 @app.route("/characters")
 def all_characters():
     return get_all_characters()
+
+@app.route("/characters/<id>")
+def character_by_id(id):
+    return get_character_by_id(id)
