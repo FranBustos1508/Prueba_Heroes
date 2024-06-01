b2ce0cba8870a45c58b7a508f0f6aaa8b7f68521
Franco <francoabustos1508@gmail.com>
2024-06-01 13:56:56 -0300

    Agregamos endpoint para eliminar personaje

--- backend/app.py
+++ backend/app.py
@@ -1,6 +1,7 @@
 from flask import Flask
 from flask_cors import CORS
-from db.characters import get_all_characters, get_character_by_id
+from db.characters import get_all_characters, get_character_by_id, remove_character
+from time import sleep
 
 app = Flask(__name__)
 CORS(app)
@@ -11,4 +12,9 @@ def all_characters():
 
 @app.route("/characters/<id>")
 def character_by_id(id):
+    sleep(2)
     return get_character_by_id(id)
+
+@app.route("/characters/<id>", methods = ["DELETE"])
+def remove_character_by_id(id):
+    return {"success": remove_character(id)}
